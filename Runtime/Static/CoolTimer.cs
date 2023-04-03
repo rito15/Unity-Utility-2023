00753b7caaa570d556d1873523dff0e358cac96e
rito15 <ghdtkals15@naver.com>
2023-04-03 21:57:46 +0900

    [2023-04-03] CoolTimer 직관성 개선, Easing 클래스 추가

--- Runtime/Static/CoolTimer.cs
+++ Runtime/Static/CoolTimer.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 /*
    [ 기록]
    2023. 03. 28. 최초 작성, 경과시간/남은시간 기능 테스트 완료
+   2023. 04. 03. 객체 생성 직관성을 위해 중간 클래스 Create 추가, IsRunning() 메소드 추가
 */
 namespace Rito.ut23
 {
     /// <summary>
     /// <para/> 지속시간/쿨타임 체크 타이머 
     /// <para/> [생성]
-    /// <para/> 1. Scaled(초) : 타임스케일 영향 받는 타이머 생성
-    /// <para/> 2. Unscaled(초) : 타임스케일 영향 받지 않는 타이머 생성
+    /// <para/> 1. Create.Scaled(초) : 타임스케일 영향 받는 타이머 생성
+    /// <para/> 2. Create.Unscaled(초) : 타임스케일 영향 받지 않는 타이머 생성
     /// <para/> 
     /// <para/> [API - 공통]
-    /// <para/> - IsEnded(): 종료 여부
+    /// <para/> - IsEnded()  : 종료 여부
+    /// <para/> - IsRunning(): 진행 여부
     /// <para/> 
     /// <para/> [API - 지속시간]
     /// <para/> - GetElapsedTime(): 경과시간(초)
@@ -31,17 +33,18 @@ namespace Rito.ut23
     /// </summary>
     public static class CoolTimer
     {
-        /// <summary> 타임스케일 영향 O </summary>
-        public static ScaledTimer Scaled(float seconds) => new ScaledTimer(seconds);
-        /// <summary> 타임스케일 영향 O </summary>
-        public static ScaledTimer GameTime(float seconds) => new ScaledTimer(seconds);
+        public static class Create
+        {
+            /// <summary> 타임스케일 영향 O </summary>
+            public static ScaledTimer Scaled(float seconds) => new ScaledTimer(seconds);
+            /// <summary> 타임스케일 영향 O </summary>
+            public static ScaledTimer GameTime(float seconds) => new ScaledTimer(seconds);
 
-
-        /// <summary> 타임스케일 영향 X </summary>
-        public static UnscaledTimer Unscaled(float seconds) => new UnscaledTimer(seconds);
-        /// <summary> 타임스케일 영향 X </summary>
-        public static UnscaledTimer Realtime(float seconds) => new UnscaledTimer(seconds);
-
+            /// <summary> 타임스케일 영향 X </summary>
+            public static UnscaledTimer Unscaled(float seconds) => new UnscaledTimer(seconds);
+            /// <summary> 타임스케일 영향 X </summary>
+            public static UnscaledTimer Realtime(float seconds) => new UnscaledTimer(seconds);
+        }
 
         /// <summary> 타임스케일 영향 O </summary>
         public readonly struct ScaledTimer
@@ -126,6 +129,12 @@ namespace Rito.ut23
             public bool IsEnded()
             {
                 return GetElapsedTime() > _duration;
+            }
+
+            /// <summary> 진행 여부 </summary>
+            public bool IsRunning()
+            {
+                return GetElapsedTime() <= _duration;
             }
         }
 
@@ -213,6 +222,12 @@ namespace Rito.ut23
             public bool IsEnded()
             {
                 return GetElapsedTime() > _duration;
+            }
+
+            /// <summary> 진행 여부 </summary>
+            public bool IsRunning()
+            {
+                return GetElapsedTime() <= _duration;
             }
         }
 
